a60c46bb2e5776a1143588cd2877849bbffc8ea9
Stanislav Romanov <kaermorchen@gmail.com>
2021-12-16 13:08:24 +0300

    Add Tailwind

---
+++ app/styles/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./app/index.html', './app/**/*.hbs'],
+  darkMode: 'media',
+  theme: {
+    extend: {
+      spacing: {
+        base: '1.5rem',
+      },
+    },
+  },
+  plugins: [],
+};
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,21 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
+    postcssOptions: {
+      compile: {
+        plugins: [
+          {
+            module: require('postcss-import'),
+          },
+          {
+            module: require('tailwindcss'),
+            options: {
+              config: './app/styles/tailwind.config.js',
+            },
+          },
+        ],
+      },
+    },
     'ember-cli-babel': {
       enableTypeScriptTransform: true,
       presets: ['@babel/preset-typescript'],
