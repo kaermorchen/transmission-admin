3bfd84c3d04af46c99609a51255b86e9f1a7e818
Stanislav Romanov <kaermorchen@gmail.com>
2021-12-17 10:16:21 +0300

    Add redirect to `torrents` route

--- app/routes/torrents.js
+++ app/routes/torrents.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
-export default class Torrents extends Route {
+export default class TorrentsRoute extends Route {
   @service store;
 
   model() {
