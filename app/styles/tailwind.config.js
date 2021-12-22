3e7f92db3946a51b5d32d1f6fe083d47525f334d
Stanislav Romanov <kaermorchen@gmail.com>
2021-12-22 15:32:47 +0300

    Add basic colors

--- app/styles/tailwind.config.js
+++ app/styles/tailwind.config.js
@@ -1,10 +1,16 @@
 module.exports = {
-  content: ['./app/index.html', './app/**/*.hbs'],
+  content: ['../index.html', '../**/*.hbs'],
   darkMode: 'media',
   theme: {
+    colors: {
+      primary: '#75b6e7',
+      basic: '#ffffff', //000000
+      neutral: '#bebebe', //999999 666666
+      bg: '#262626', //ffffff
+    },
     extend: {
       spacing: {
-        base: '1.5rem',
+        basic: '1.5rem',
       },
     },
   },
