f5e14ed3a43ca23ef719581318255b7631f07497
Stanislav Romanov <kaermorchen@gmail.com>
2021-12-25 09:53:22 +0300

    Add status of torrent

--- app/models/torrent.js
+++ app/models/torrent.js
@@ -1,5 +1,20 @@
 import Model, { attr } from '@ember-data/model';
 
+const statusNames = new Map([
+  [0, 'Torrent is stopped'],
+  [1, 'Queued to check files'],
+  [2, 'Checking files'],
+  [3, 'Queued to download'],
+  [4, 'Downloading'],
+  [5, 'Queued to seed'],
+  [6, 'Seeding'],
+]);
+
 export default class TorrentModel extends Model {
   @attr('string') name;
+  @attr('number') status;
+
+  get statusName() {
+    return statusNames.get(this.status) ?? null;
+  }
 }
